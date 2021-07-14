81af8eb7d3673cae0286b1552fe8b04b10bce4d1
Manlx <74509303+Manlx@users.noreply.github.com>
2021-07-15 01:48:57 +0200

    Update Source.cpp

--- ConsoleFormat/Source.cpp
+++ ConsoleFormat/Source.cpp
@@ -19,62 +19,70 @@ public:
         ArrSize = R * C;
         CharArr = new char[ArrSize ];
     }
+    void ChangeValue(unsigned int Row, unsigned int Col, char Value)
+    {
+        this->CharArr[Row * this->Cols + Col] = Value;
+    }
+    void GenDisplay()
+    {
+        //Clears Console
+        Log("\x1B[2J\x1B[H");
+        //std::system("cls");
+        //Creats Top aplha Index
+        int PreSize = this->Cols * 3;
+        char Opening = 'A';
+        char* OpeningString = new char[PreSize];
+        int Counter = 0;
+        while (Counter < PreSize)
+        {
+            OpeningString[Counter++] = '|';
+            OpeningString[Counter++] = Opening++;
+            OpeningString[Counter++] = '|';
+        }
+        OpeningString[PreSize] = 0;
+        printf("%-2s%s\n", "  ", OpeningString);
+
+        for (int x = 0; x < this->Rows; x++)
+        {
+            Counter = 0;
+            int ArrReader = x * this->Cols;
+            while (Counter < PreSize)
+            {
+                OpeningString[Counter++] = '|';
+                OpeningString[Counter++] = this->CharArr[ArrReader++];
+                OpeningString[Counter++] = '|';
+            }
+            OpeningString[Counter] = 0;
+            printf("%-2d%s\n", x, OpeningString);
+        }
+    }
     ~DispGrid()
     {
         delete[] CharArr;
     }
 };
-void GenDisplay()
+
+char CToUp(char cIn)
 {
-    
-    printf("  |A||B||C||D||E||F||G|\n");
-    for (uint16_t x = 0; x < 10; x++)
-        printf("%-2d%s\n", x, "|#||#||#||#||#||#||#|");
-}
-void GenDisplay(DispGrid* GridDisp)
-{
-    //Creats Top aplha Index
-    DispGrid Temp = *GridDisp;
-    int PreSize = Temp.ArrSize * 3 + 3;
-    char Opening = 'A';
-    char* OpeningString = new char[PreSize];
-    int Counter = 0;
-    OpeningString[Counter++] = ' ';
-    OpeningString[Counter++] = ' ';
-    while (Counter < PreSize)
-    {
-        OpeningString[Counter++] = '|';
-        OpeningString[Counter++] = Opening++;
-        OpeningString[Counter++] = '|';
-    }
-        
-    OpeningString[PreSize-1] = 0;
-    printf("%s\n",OpeningString);
-    Counter = 0;
-    PreSize -= 3;
-    for (int x = 0; x < ArrLay.RowSize; x++)
-    {
-        int ArrReader = 0;
-        while (Counter < PreSize)
-        {
-            OpeningString[Counter++] = '|';
-            OpeningString[Counter++] = arr[ArrReader++];
-            OpeningString[Counter++] = '|';
-        }
-        OpeningString[Counter] = 0;
-        printf("%-2d%s\n",x,OpeningString);
-    }
-    //delete[] OpeningString;
+    if (cIn > 90)
+        return cIn - 32;
 }
 int main()
 {
-    LayOut ArrLayout;
-    ArrLayout.ColSize = 10;
-    ArrLayout.RowSize = 10;
-    int ArrSize = ArrLayout.ColSize * ArrLayout.RowSize;
-    char* ArrPtr = new char[ArrSize];
-    
-    GenDisplay(ArrPtr,ArrLayout);
-    delete[] ArrPtr;
+    DispGrid MainGrid(10,10);
+
+    MainGrid.GenDisplay();
+    char cCol,cValue;
+    unsigned int iRow,iCol;
+    std::cout << "Select Colom: ";
+    std::cin >> cCol;
+    cCol = CToUp(cCol);
+    iCol = cCol - 'A';
+    std::cout << "Select Row: ";
+    std::cin >> iRow;
+    std::cout << "Value to to Change: ";
+    std::cin >> cValue;
+    MainGrid.ChangeValue(iRow, iCol,cValue);
+    MainGrid.GenDisplay();
     return 0;
 }
